919d39da383c1c3f92e67d951709c6eeeded1aab
DewP <parinsura@gmail.com>
2022-07-08 16:38:59 +0700

    speech test

---
+++ src/x/scripts/sr.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+import speech_recognition as sr
+import time
+
+def speechreg():
+    rospy.init_node('x-sr', anonymous=True)
+    while not rospy.is_shutdown():
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+        
+        with sr.Microphone() as source:
+            print("[SR]> speechreg # Say something!")
+            audio = r.record(source, duration=4)
+            
+        # recognize speech using Google Speech Recognition
+        try:
+            result = r.recognize_google(audio).lower()
+            print("[SR]> speechreg # result: " + result)
+            pub = rospy.Publisher('/tts_input', String, queue_size=10)
+            sentense = 'you said ' + result
+            pub.publish(sentense)
+        except sr.UnknownValueError:
+            print("[SR]> speechreg # could not understand audio")
+        except sr.RequestError as e:
+            print("[SR]> speechreg # Could not request results from Google Speech Recognition service; {0}".format(e))
+        
+
+if __name__ == '__main__':
+    try:
+        speechreg()
+    except rospy.ROSInterruptException:
+        pass
---
+++ src/x/scripts/tts.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+from gtts import gTTS
+import os
+
+def callback(data):
+    rospy.loginfo("[TTS]> Input: %s", data.data)
+
+    text = data.data
+    tts = gTTS(text)
+    
+    tts.save("speech.mp3")
+    os.system("mpg321 speech.mp3")
+    os.remove("speech.mp3")
+    
+def googletts():
+    rospy.init_node('x-tts', anonymous=True)
+
+    rospy.Subscriber("/tts_input", String, callback)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    googletts()
