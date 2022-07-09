797a953f6eb2c1543faa5acb6d0225a41b00fd60
DewP <parinsura@gmail.com>
2022-07-10 01:51:40 +0700

    Prepare get drink action

--- src/projectx/scripts/jane/jane_sr.py
+++ src/projectx/scripts/jane/jane_sr.py
@@ -14,7 +14,8 @@ class JaneSR:
     0 = idle, listen to hey jane
     1 = taking photo
     2 = telling a joke
-    3 = grab a drink
+    3 = select a drink menu
+    4 = getting a drink at the kitchen
     '''
     state = 0
 
@@ -40,6 +41,8 @@ class JaneSR:
                     self.state_2(result)
                 elif self.state == 3:
                     self.state_3(result)
+                elif self.state == 4:
+                    self.state_4(result)
             except sr.UnknownValueError:
                 print("[SR]> speechreg # could not understand audio")
             except sr.RequestError as e:
@@ -53,10 +56,17 @@ class JaneSR:
         print('on_take_photo_finished')
         self.state = 0
 
+    def on_get_drink_finished(self, data):
+        print('on_get_drink_finished')
+        self.state = 0
+
     # idle
     def state_0(self, sentense):
         rospy.loginfo('state 0 # sentense %s', sentense)
         if (
+            sentense.startswith('jane') or
+            sentense.startswith('jan') or
+            sentense.startswith('jen') or
             sentense.startswith('hey jane') or
             sentense.startswith('hey jan') or
             sentense.startswith('hey jen') or
@@ -72,9 +82,11 @@ class JaneSR:
                 print('state 0 # command get joke')
                 self.get_joke_pub.publish('')
                 self.state = 2
-            elif 'grab' in sentense or 'drink' in sentense:
+            elif 'grab' in sentense or 'drink' in sentense or 'thirsty' in sentense:
                 print('state 0 # command grab a drink')
                 # get drink
+                self.tts_pub.publish('would you like some tea or coffee?')
+                rospy.sleep(2)
                 self.state = 3
             else:
                 print('state 0 # unknown command')
@@ -83,17 +95,35 @@ class JaneSR:
 
     # taking a phto
     def state_1(self, sentense):
-        print('state 1')
+        print('state 1 # taking a photo')
+        # do nothing
 
     # telling a joke
     def state_2(self, sentense):
-        print('state 2')
+        print('state 2 # telling a joke')
+        # do nothing
     
-    # grab a drink
+    # select a drink menu
     def state_3(self, sentense):
-        print('state 3')
+        rospy.loginfo('state 3 # selecting drink menu: sentense %s', sentense)
+        if 'tea' in sentense:
+            self.tts_pub.publish('I will get you a cup of tea')
+            rospy.sleep(4)
+        elif 'coffee' in sentense:
+            self.tts_pub.publish('I will buy it at Starbucks, please wait.')
+            rospy.sleep(4)
+        else:
+            self.tts_pub.publish('Just a drinking water then')
+            rospy.sleep(4)
+        # todo nav to kitchen then back
+        self.state = 4
 
-
+    # getting a drink at the kitchen
+    def state_4(self, sentense):
+        print('state 4')
+        self.tts_pub.publish('I am getting you a drink, please wait')
+        rospy.sleep(4)
+    
 if __name__ == '__main__':
     rospy.init_node('x_jane_sr', anonymous=False)
     JaneSR().start()
