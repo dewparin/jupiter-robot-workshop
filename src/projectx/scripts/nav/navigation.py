bc790a8a61f7dfd7463ed18b995bde9f5d47bc02
DewP <parinsura@gmail.com>
2022-07-09 17:58:56 +0700

    nav cont

--- src/projectx/scripts/nav/navigation.py
+++ src/projectx/scripts/nav/navigation.py
@@ -10,6 +10,9 @@ from tf.transformations import quaternion_from_euler
 from std_msgs.msg import String
 
 class NavToPoint:
+
+    target = 0
+    
     def __init__(self):
         rospy.on_shutdown(self.cleanup)
 
@@ -37,6 +40,9 @@ class NavToPoint:
         rospy.Subscriber('/jane_nav', String, self.nav_callback)
         rospy.loginfo("Ready to go")
 
+    def update_initial_pose(self, initial_pose):
+        self.initial_pose = initial_pose
+
     def cleanup(self):
         rospy.loginfo("Shutting down navigation	....")
         self.move_base.cancel_goal()
@@ -46,8 +52,35 @@ class NavToPoint:
         rospy.loginfo('[NAV] target = %s', target)
         if target == 'kitchen':
             print('[NAV] goto kitchen')
+            self.target = 1
         else:
             print('[NAV] unknown target')
+            self.target = 0
+
+    def run_loop(self):
+        A_theta = 1.5708
+        quaternion = quaternion_from_euler(0.0, 0.0, A_theta)
+        kitchen_point = Pose(Point(1.0, 1.0, 0.000), Quaternion(quaternion[0], quaternion[1], quaternion[2], quaternion[3]))
+        self.goal = MoveBaseGoal()
+
+        while not rospy.is_shutdown():
+            self.goal.target_pose.header.frame_id = 'map'
+            self.goal.target_pose.header.stamp = rospy.Time.now()
+
+            if self.target == 1:
+                rospy.loginfo("Going to kitchen")
+                rospy.sleep(2)
+                self.goal.target_pose.pose = kitchen_point
+                self.move_base.send_goal(self.goal)
+                waiting = self.move_base.wait_for_result(rospy.Duration(300))
+                if waiting == 1:
+                    rospy.loginfo("Reached point A")
+                    rospy.sleep(2)
+                    rospy.loginfo("Ready to go back")
+                    rospy.sleep(2)
+                    self.target = 0
+            
+            rospy.Rate(5).sleep()
 
 
 if __name__=="__main__":
